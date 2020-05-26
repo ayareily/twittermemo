164366d60004aecaa1aabfb3c4172414356edd5d
ayareily <musiclear05@gmail.com>
2020-05-26 16:24:11 +0900

    first commit

---
+++ login.php
@@ -0,0 +1,17 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+
+    <h1>memoにログイン</h1>
+    <input type="text" name="name">ユーザー名<br>
+    <input type="password" name="password">パスワード
+</body>
+</html>
